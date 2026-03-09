a60a3400a280b7e909410f3032bcb82042cc5a76
kingsley <kchukwuma28@gmail.com>
2026-03-09 14:56:25 +0100

    redirect

--- server.js
+++ server.js
@@ -1083,7 +1083,7 @@ app.post("/payment/initiate", async (req, res) => {
         email,
         amount: total * 100,
         metadata: { delivery_option_id, delivery_location },
-        callback_url: "https://cs-lux-production.up.railway.app//payment/verify",
+        callback_url: "https://cs-lux-production.up.railway.app/payment/verify",
       },
       { headers: paystackHeaders }
     );
